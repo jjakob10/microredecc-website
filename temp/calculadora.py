f90bf3bf9bc93caecaa4600a4628bf8ebb2fca4d
beatriz610 <bibia.msilva14@gmail.com>
2021-05-02 16:26:26 -0300

    Co-authored-by: jjakob10 <jjakob10@users.noreply.github.com>

---
+++ temp/calculadora.py
@@ -0,0 +1,31 @@
+#duty cycle(ton/T); rds(on); id(on); frequencia; tr; tf; Vds(off); temperatura junção; temperatura ambiente; Rcd; Rjc; fator de correção de temperatura e fator de correção comprimento
+
+def potcom(f, tr, tf, id, Voff): 
+    p= (f/2)*(tr+tf)*id*Voff
+    return p
+
+def potcond( ton, T, rds, id):
+    p= (ton/T)*rds*id*id  
+    return p
+      
+def rja(tj, tam, p1, p2):
+    rja= (tj-tam)/ (p1+p2)
+    return rja
+
+def rda( rja, rcd, rjc):
+    rda= rja-rcd-rjc
+    return rda
+
+ def disscor( rda, ft,fc):
+     disscor= rda*ft*fc
+     return disscor
+
+
+
+def aocup( afio, voltas):
+    aocup= afio* voltas
+    return aocup
+
+def comparar(anuc, aocup):
+    r= anuc> aocup*2
+    return r
